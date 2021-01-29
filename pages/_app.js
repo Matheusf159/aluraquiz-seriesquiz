d3ecb0bc3d7e9de27d104126dce8421002999d04
Matheus <matheusfellype772@gmail.com>
2021-01-29 12:30:04 -0400

    quinta aula imersao react

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>{db.title}</title>
         <meta property="og:title" content={db.title} />
-        <meta property="og:image" content={db.bg} />
+        <meta property="og:image" content="https://wallpapercave.com/wp/wp3421271.jpg" />
         <meta property="og:description" content={db.description} />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" /> 
